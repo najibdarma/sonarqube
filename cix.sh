828d5aacb1b2d47474308163793328fa86d553a5
David Rautureau <david.rautureau@sonarsource.com>
2018-02-20 19:07:00 +0100

    TO REMOVE Run all ITs with Gradle

--- cix.sh
+++ cix.sh
@@ -14,25 +14,11 @@ case "$RUN_ACTIVITY" in
     DB_ENGINE=$(sed "s/run-db-integration-tests-//g" <<< $RUN_ACTIVITY | cut -d \- -f 1)
     CATEGORY_GROUP=$(sed "s/run-db-integration-tests-//g" <<< $RUN_ACTIVITY | cut -d \- -f 2)
 
-    if [[ "$GITHUB_BRANCH" != "PULLREQUEST-"* ]] && [[ "$GITHUB_BRANCH" != "master" ]] && [[ "$GITHUB_BRANCH" != "branch-"* ]] && [[ "$GITHUB_BRANCH" != "dogfood-on-next" ]]; then
-      # do not execute QA tests on feature branch outside pull request
-      exit 0
-
-    elif [[ "$GITHUB_BRANCH" == "PULLREQUEST-"* ]] && [[ "$DB_ENGINE" != "postgresql93" ]]; then
-      # restrict QA tests to PostgreSQL on pull requests
-      exit 0
-
-    elif [[ "$GITHUB_BRANCH" == "dogfood-on-next" ]] && [[ "$DB_ENGINE" != "postgresql93" ]]; then
-      # restrict QA tests to PostgreSQL on dogfood branch
-      exit 0
-
-    else
       ./gradlew --no-daemon --console plain -i \
           :tests:integrationTest \
           -Dcategory="$CATEGORY" \
           -Dorchestrator.configUrl="http://infra.internal.sonarsource.com/jenkins/orch-$DB_ENGINE.properties" \
           -Dcix="true"
-    fi
     ;;
 
   run-it-released-plugins)
@@ -40,23 +26,12 @@ case "$RUN_ACTIVITY" in
     ;;
 
   run-perf-tests)
-    if [[ "$GITHUB_BRANCH" == "PULLREQUEST-"* ]]; then
-        # do not execute Perf tests on feature branch outside pull request
-        exit 0
-    else
       ./run-perf-tests.sh
-    fi
     ;;
 
   run-upgrade-tests-*)
     DB_ENGINE=$(sed "s/run-upgrade-tests-//g" <<< $RUN_ACTIVITY)
-    if [[ "$GITHUB_BRANCH" != "master" ]] && [[ "$GITHUB_BRANCH" != "branch-"* ]] && [[ "$DB_ENGINE" != "postgresql93" ]]; then
-      # restrict upgrade tests to PostgreSQL on feature branches and dogfood
-      exit 0
-
-    else    
       ./run-upgrade-tests.sh "http://infra.internal.sonarsource.com/jenkins/orch-${DB_ENGINE}.properties"
-    fi
     ;;
 
   *)
